42a4b4e52773cba659862fe5dd59dd415bf85270
Lucas <lucas.alleaume@iia-formation.fr>
2024-11-17 12:57:19 +0100

    Reservation completion

---
+++ src/Entity/Reservation.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationRepository::class)]
+class Reservation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $reference = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateStart = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateEnd = null;
+
+    #[ORM\Column]
+    private ?int $numberRentalDay = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $totalCost = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    private ?Customer $customer = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    private ?Vehicle $vehicle = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    private ?State $state = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): static
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getDateStart(): ?\DateTimeInterface
+    {
+        return $this->dateStart;
+    }
+
+    public function setDateStart(\DateTimeInterface $dateStart): static
+    {
+        $this->dateStart = $dateStart;
+
+        return $this;
+    }
+
+    public function getDateEnd(): ?\DateTimeInterface
+    {
+        return $this->dateEnd;
+    }
+
+    public function setDateEnd(\DateTimeInterface $dateEnd): static
+    {
+        $this->dateEnd = $dateEnd;
+
+        return $this;
+    }
+
+    public function getNumberRentalDay(): ?int
+    {
+        return $this->numberRentalDay;
+    }
+
+    public function setNumberRentalDay(int $numberRentalDay): static
+    {
+        $this->numberRentalDay = $numberRentalDay;
+
+        return $this;
+    }
+
+    public function getTotalCost(): ?string
+    {
+        return $this->totalCost;
+    }
+
+    public function setTotalCost(string $totalCost): static
+    {
+        $this->totalCost = $totalCost;
+
+        return $this;
+    }
+
+    public function getCustomer(): ?Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(?Customer $customer): static
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
+
+    public function getVehicle(): ?Vehicle
+    {
+        return $this->vehicle;
+    }
+
+    public function setVehicle(?Vehicle $vehicle): static
+    {
+        $this->vehicle = $vehicle;
+
+        return $this;
+    }
+
+    public function getState(): ?State
+    {
+        return $this->state;
+    }
+
+    public function setState(?State $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
